10f3ecd6b86837ed76fe578dc393c150211fbea2
Siyu <nimagangfour@gmail.com>
2019-08-12 22:54:24 +1000

    redux-basic-settle done

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,6 +3,30 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
+  if (action.type === "INCREMENT") {
+    return {
+      ...state,
+      counter: state.counter + 1
+    };
+  }
+  if (action.type === "DECREMENT") {
+    return {
+      ...state,
+      counter: state.counter - 1
+    };
+  }
+  if (action.type === "ADD_5") {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    };
+  }
+  if (action.type === "SUBTRACT_5") {
+    return {
+      ...state,
+      counter: state.counter - action.value
+    };
+  }
   return state;
 };
 
